8230162d11604ec2fbb54e56ac5faed84e8813ca
Campbell Lowman <campbell@cyberhilpartners.com>
2025-08-27 14:14:39 -0400

    save commit

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import os
 from data_loaders.s3_data_loader import S3BatchDownloader
+from langchain import hub
+from langchain_core.documents import Document
 from langchain.chat_models import init_chat_model
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_postgres import PGVector
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langgraph.graph import START, StateGraph
+from langchain_core.prompts import PromptTemplate
+from typing_extensions import List, TypedDict
 
 
 
@@ -24,29 +29,34 @@ S3_BUCKET = "pinpointmigration2"
 data_loader = S3BatchDownloader(bucket=S3_BUCKET, dest_dir="data/", prefix=None, chunk_size=5, state_path=".s3_progress.json")
 
 
-resumes_processed = 0
-while not data_loader.done() and resumes_processed < 1000:
-    pdfs_pulled = data_loader.get_next_chunk()
-    # print(f"Downloaded {len(pdfs_pulled)} files from S3 bucket '{S3_BUCKET}' to 'data/' directory.")
+def run_pipeline():
+    while not data_loader.done():
+        pdfs_pulled = data_loader.get_next_chunk()
+        # print(f"Downloaded {len(pdfs_pulled)} files from S3 bucket '{S3_BUCKET}' to 'data/' directory.")
 
-    for pdf_pulled in pdfs_pulled:
-        pdf_path = pdf_pulled[0]
-        # s3url = pdf_pulled[1]
+        for pdf_pulled in pdfs_pulled:
+            pdf_path = pdf_pulled[0]
+            # s3url = pdf_pulled[1]
 
-        loader = PyPDFLoader(pdf_path, mode="single")
-        docs = loader.load()
+            loader = PyPDFLoader(pdf_path, mode="single")
+            docs = loader.load()
 
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=2048, chunk_overlap=200)
-        all_splits = text_splitter.split_documents(docs)
-        vector_store.add_documents(documents=all_splits)
-        resumes_processed += 1
-    print(f"Processed {resumes_processed} resumes so far.")
+            text_splitter = RecursiveCharacterTextSplitter(chunk_size=2048, chunk_overlap=200)
+            all_splits = text_splitter.split_documents(docs)
+            vector_store.add_documents(documents=all_splits)
+            resumes_processed += 1
+        print(f"Processed {resumes_processed} resumes so far.")
+
+
 
 # Define prompt for question-answering
 # N.B. for non-US LangSmith endpoints, you may need to specify
 # api_url="https://api.smith.langchain.com" in hub.pull.
-prompt = hub.pull("rlm/rag-prompt")
-
+# prompt = hub.pull("rlm/rag-prompt")
+prompt = PromptTemplate.from_template("""You are an assistant for searching a database of resumes. Use the following snippets of resumes to answer the question. If you don't know the answer, just say that you don't know. Be concise but make sure to include the names of all relevant people.
+Question: {question} 
+Context: {context} 
+Answer:""")
 
 # Define state for application
 class State(TypedDict):
@@ -57,7 +67,7 @@ class State(TypedDict):
 
 # Define application steps
 def retrieve(state: State):
-    retrieved_docs = vector_store.similarity_search(state["question"])
+    retrieved_docs = vector_store.similarity_search(state["question"], k=20)
     return {"context": retrieved_docs}
 
 
@@ -80,3 +90,7 @@ def run_chat():
 
         response = graph.invoke({"question": question})
         print(response["answer"])
+
+if __name__ == "__main__":
+    run_chat()
+    # run_pipeline()
